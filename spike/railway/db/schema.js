1b253c23bf20c5e0fbe479730edd9a2ef78a19e1
Swing Zhou <swing.zhou@hotmail.com>
2012-04-03 17:03:08 +0800

    add validators for model

--- spike/railway/db/schema.js
+++ spike/railway/db/schema.js
@@ -8,3 +8,7 @@ var Sticker = describe('Sticker', function () {
     property('title', String);
     property('content', String);
 });
+
+Sticker.validatesPresenceOf('title', 'content');
+Sticker.validatesLengthOf('title', {max: 20, message: {max: 'title is too long'}});
+Sticker.validatesLengthOf('content', {max: 200, message: {max: 'content is too long'}});
